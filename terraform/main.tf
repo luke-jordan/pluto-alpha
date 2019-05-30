c61b84fae5828ffd9a9f76f6155657fc559baffc
yuriy <yuriy@supersignalcapital.com>
2019-05-30 17:42:40 +0300

    Added usage of deploy user

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@
 
 provider "aws" {
   access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  secret_key = "${var.aws_secret_access_key}"
   region     = "${var.aws_region}"
 }
 
