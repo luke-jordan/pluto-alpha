c62f6360da9162e07ef90f204f99e535b65e4e72
yuriy <yuriy@supersignalcapital.com>
2019-06-26 11:31:02 +0300

    fixed deployment package s3 target

--- terraform/lambda-insert-user-credentials.tf
+++ terraform/lambda-insert-user-credentials.tf
@@ -15,7 +15,7 @@ resource "aws_lambda_function" "user-insertion-handler" {
   tags                           = {"environment"  = "${terraform.workspace}"}
   
   s3_bucket = "pluto.lambda.${terraform.workspace}"
-  s3_key = "${var.user_insertion_handler_lambda_function_name}/${var.deploy_code_commit_hash}.zip"
+  s3_key = "auth-api/${var.deploy_code_commit_hash}.zip"
 
   environment {
     variables = {
