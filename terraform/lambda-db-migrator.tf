4d31c2386c2145dbf0f9be5be497403de51b650a
yuriy <yuriy@supersignalcapital.com>
2019-06-30 13:44:14 +0300

    Added DB migrator

--- terraform/lambda-db-migrator.tf
+++ terraform/lambda-db-migrator.tf
@@ -1,12 +1,12 @@
-variable "db_migrator_lambda_function_name" {
-  default = "db_migrator"
+variable "db_migration_lambda_function_name" {
+  default = "db_migration"
   type = "string"
 }
 
-resource "aws_lambda_function" "db_migrator" {
+resource "aws_lambda_function" "db_migration" {
 
-  function_name                  = "${var.db_migrator_lambda_function_name}"
-  role                           = "${aws_iam_role.db_migrator_role.arn}"
+  function_name                  = "${var.db_migration_lambda_function_name}"
+  role                           = "${aws_iam_role.db_migration_role.arn}"
   handler                        = "index.handler"
   memory_size                    = 256
   reserved_concurrent_executions = 20
@@ -15,7 +15,7 @@ resource "aws_lambda_function" "db_migrator" {
   tags                           = {"environment"  = "${terraform.workspace}"}
   
   s3_bucket = "pluto.lambda.${terraform.workspace}"
-  s3_key = "${var.db_migrator_lambda_function_name}/${var.deploy_code_commit_hash}.zip"
+  s3_key = "${var.db_migration_lambda_function_name}/${var.deploy_code_commit_hash}.zip"
 
   environment {
     variables = {
@@ -43,11 +43,11 @@ resource "aws_lambda_function" "db_migrator" {
     security_group_ids = [aws_security_group.sg_5432_egress.id, aws_security_group.sg_db_access_sg.id, aws_security_group.sg_https_dns_egress.id]
   }
 
-  depends_on = [aws_cloudwatch_log_group.db_migrator]
+  depends_on = [aws_cloudwatch_log_group.db_migration]
 }
 
-resource "aws_iam_role" "db_migrator_role" {
-  name = "${var.db_migrator_lambda_function_name}_role"
+resource "aws_iam_role" "db_migration_role" {
+  name = "${var.db_migration_lambda_function_name}_role"
 
   assume_role_policy = <<EOF
 {
@@ -66,42 +66,42 @@ resource "aws_iam_role" "db_migrator_role" {
 EOF
 }
 
-resource "aws_cloudwatch_log_group" "db_migrator" {
-  name = "/aws/lambda/${var.db_migrator_lambda_function_name}"
+resource "aws_cloudwatch_log_group" "db_migration" {
+  name = "/aws/lambda/${var.db_migration_lambda_function_name}"
 
   tags = {
     environment = "${terraform.workspace}"
   }
 }
 
-resource "aws_iam_role_policy_attachment" "db_migrator_basic_execution_policy" {
-  role = "${aws_iam_role.db_migrator_role.name}"
+resource "aws_iam_role_policy_attachment" "db_migration_basic_execution_policy" {
+  role = "${aws_iam_role.db_migration_role.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_iam_role_policy_attachment" "db_migrator_vpc_execution_policy" {
-  role = "${aws_iam_role.db_migrator_role.name}"
+resource "aws_iam_role_policy_attachment" "db_migration_vpc_execution_policy" {
+  role = "${aws_iam_role.db_migration_role.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
 
 ////////////////// CLOUD WATCH ///////////////////////////////////////////////////////////////////////
 
-resource "aws_cloudwatch_log_metric_filter" "fatal_metric_filter_db_migrator" {
-  log_group_name = "${aws_cloudwatch_log_group.db_migrator.name}"
+resource "aws_cloudwatch_log_metric_filter" "fatal_metric_filter_db_migration" {
+  log_group_name = "${aws_cloudwatch_log_group.db_migration.name}"
   metric_transformation {
-    name = "${var.db_migrator_lambda_function_name}_fatal_api_alarm"
+    name = "${var.db_migration_lambda_function_name}_fatal_api_alarm"
     namespace = "lambda_errors"
     value = "1"
   }
-  name = "${var.db_migrator_lambda_function_name}_fatal_api_alarm"
+  name = "${var.db_migration_lambda_function_name}_fatal_api_alarm"
   pattern = "FATAL_ERROR"
 }
 
-resource "aws_cloudwatch_metric_alarm" "fatal_metric_alarm_db_migrator" {
-  alarm_name = "${var.db_migrator_lambda_function_name}_fatal_api_alarm"
+resource "aws_cloudwatch_metric_alarm" "fatal_metric_alarm_db_migration" {
+  alarm_name = "${var.db_migration_lambda_function_name}_fatal_api_alarm"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods = 1
-  metric_name = "${aws_cloudwatch_log_metric_filter.fatal_metric_filter_db_migrator.name}"
+  metric_name = "${aws_cloudwatch_log_metric_filter.fatal_metric_filter_db_migration.name}"
   namespace = "lambda_errors"
   period = 60
   threshold = 0
@@ -109,22 +109,22 @@ resource "aws_cloudwatch_metric_alarm" "fatal_metric_alarm_db_migrator" {
   alarm_actions = ["${aws_sns_topic.fatal_errors_topic.arn}"]
 }
 
-resource "aws_cloudwatch_log_metric_filter" "security_metric_filter_db_migrator" {
-  log_group_name = "${aws_cloudwatch_log_group.db_migrator.name}"
+resource "aws_cloudwatch_log_metric_filter" "security_metric_filter_db_migration" {
+  log_group_name = "${aws_cloudwatch_log_group.db_migration.name}"
   metric_transformation {
-    name = "${var.db_migrator_lambda_function_name}_security_api_alarm"
+    name = "${var.db_migration_lambda_function_name}_security_api_alarm"
     namespace = "lambda_errors"
     value = "1"
   }
-  name = "${var.db_migrator_lambda_function_name}_security_api_alarm"
+  name = "${var.db_migration_lambda_function_name}_security_api_alarm"
   pattern = "SECURITY_ERROR"
 }
 
-resource "aws_cloudwatch_metric_alarm" "security_metric_alarm_db_migrator" {
-  alarm_name = "${var.db_migrator_lambda_function_name}_security_api_alarm"
+resource "aws_cloudwatch_metric_alarm" "security_metric_alarm_db_migration" {
+  alarm_name = "${var.db_migration_lambda_function_name}_security_api_alarm"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods = 1
-  metric_name = "${aws_cloudwatch_log_metric_filter.security_metric_filter_db_migrator.name}"
+  metric_name = "${aws_cloudwatch_log_metric_filter.security_metric_filter_db_migration.name}"
   namespace = "lambda_errors"
   period = 60
   threshold = 0
