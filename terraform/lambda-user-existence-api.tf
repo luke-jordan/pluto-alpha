d0adf36d56ac8ab99ee94863a45d49a1154c33e2
yuriy <yuriy@supersignalcapital.com>
2019-06-30 11:05:29 +0300

    Added Dynamo Access role

--- terraform/lambda-user-existence-api.tf
+++ terraform/lambda-user-existence-api.tf
@@ -86,7 +86,6 @@ resource "aws_cloudwatch_log_group" "user_existence_api" {
   }
 }
 
-
 resource "aws_iam_role_policy_attachment" "user_existence_api_basic_execution_policy" {
   role = "${aws_iam_role.user_existence_api_role.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
