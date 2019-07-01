21e90044eebd127f4a4e6c772d96058eb0216d4e
yuriypo <ypolonskiy@gmail.com>
2019-07-01 11:07:36 +0300

    Added codecov

--- functions/test_all.sh
+++ functions/test_all.sh
@@ -3,7 +3,7 @@
 echo "Running tests and outputing coverage reports"
 cd user-existence-api; npm test; npm run-script generate-coverage
 cd ../user-activity-api; npm test; npm run-script generate-coverage
-cd ../float-api; npm test; run-script generate-coverage
+cd ../float-api; npm test; npm run-script generate-coverage
 
 echo "Uploading coverage reports"
 codecov
