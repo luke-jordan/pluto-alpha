3c0b90e258db81187ba3d23e8baade191a2ffcc3
yuriypo <ypolonskiy@gmail.com>
2019-07-02 23:38:45 +0300

    RDS user and password from ENV

--- terraform/lambda-db-migrator.tf
+++ terraform/lambda-db-migrator.tf
@@ -27,8 +27,8 @@ resource "aws_lambda_function" "db_migration" {
                   "region": "${var.aws_default_region[terraform.workspace]}"
               },
               "db": {
-                  "host": terraform.workspace == "staging" ? aws_rds_cluster.pg_rds[0].endpoint : aws_db_instance.rds[0].address,
-                  "port": terraform.workspace == "staging" ? aws_rds_cluster.pg_rds[0].port : aws_db_instance.rds[0].port,
+                  "host": "${terraform.workspace == "staging" ? aws_rds_cluster.pg_rds[0].endpoint : aws_db_instance.rds[0].address}",
+                  "port": "${terraform.workspace == "staging" ? aws_rds_cluster.pg_rds[0].port : aws_db_instance.rds[0].port}",
                   "database": "${var.db_name}",
                   "user": "${var.db_user}",
                   "password": "${var.db_password}"
