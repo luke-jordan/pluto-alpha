a0f8411b5764acf0f8fd1f3bda3622a802c2bf26
Busani <busani@plutosaving.com>
2019-08-09 18:15:54 +0200

    Improved user message instruction insertion lambda readability

--- functions/user-notifications-api/create-msg-instruction.js
+++ functions/user-notifications-api/create-msg-instruction.js
@@ -6,48 +6,17 @@ const RdsConnection = require('rds-common');
 
 const rdsConnection = new RdsConnection(config.get('db'))
 
+
 module.exports.createMsgInstructions = async (event) => {
     try {
         logger('msg instruction inserter received:', event);
         const params = event; // normalise event
-
-        const persistableObject = {
-            presentationType: params.presentationType,
-            active: true,
-            audienceType: params.audienceType,
-            templates: {
-                default: params.defaultTemplate,
-                otherTemplates: params.otherTemplates? params.otherTemplates: null
-            },
-            selectionInstruction: params.selectionInstruction? params.selectionInstruction: null,
-            recurrenceInstruction: params.recurrenceInstruction? params.recurrenceInstruction: null,
-            responseAction: params.responseAction? params.responseAction: null,
-            responseContext: params.responseContext? params.responseContext: null,
-            startTime: params.startTime? params.startTime: null,
-            endTime: params.endTime? params.endTime: null,
-            priority: params.priority? params.priority: 0
-        };
+        const persistableObject = createPersistableObject(params);
         logger('created persistable object:', persistableObject);
         const instructionEvalResult = exports.evaluateMessageInstruction(persistableObject);
         logger('Message instruction evaluation resulted in:', instructionEvalResult);
 
-        const insertionQueryArray = [
-            'presentation_type',
-            'active',
-            'audience_type',
-            'templates',
-            'selection_instruction',
-            'recurrence_instruction',
-            'response_action',
-            'start_time',
-            'end_time',
-            'priority'
-        ];
-        // to be moved to persistence/ directory
-        const insertionQuery = `insert into ${config.get('tables.messageInstructionTable')} (${insertionQueryArray.join(', ')}) values %L returning insertion_id, creation_time`;
-        const insertionColumns = '${presentationType} ${active} ${audienceType} ${templates} ${selectionInstruction} ${recurrenceInstruction} ${responseAction} ${responseContext} ${startTime} ${endTime} ${priority}'; 
-        const insertArray = [persistableObject];
-        const databaseResponse = await rdsConnection.insertRecords(insertionQuery, insertionColumns, insertArray);
+        const databaseResponse = await insertMessageInstruction(persistableObject)
         logger('Recieved this back from message instruction insertion:', databaseResponse);
         return databaseResponse.rows
     } catch (err) {
@@ -55,6 +24,48 @@ module.exports.createMsgInstructions = async (event) => {
         return { error: err.message };
     }
 };
+
+
+// to be moved to persistence/ directory
+const insertMessageInstruction = async (persistableObject) => {
+    const insertionQueryArray = [
+        'presentation_type',
+        'active',
+        'audience_type',
+        'templates',
+        'selection_instruction',
+        'recurrence_instruction',
+        'response_action',
+        'start_time',
+        'end_time',
+        'priority'
+    ];
+    
+    const insertionQuery = `insert into ${config.get('tables.messageInstructionTable')} (${insertionQueryArray.join(', ')}) values %L returning insertion_id, creation_time`;
+    const insertionColumns = '${presentationType} ${active} ${audienceType} ${templates} ${selectionInstruction} ${recurrenceInstruction} ${responseAction} ${responseContext} ${startTime} ${endTime} ${priority}'; 
+    const insertArray = [persistableObject];
+    const databaseResponse = await rdsConnection.insertRecords(insertionQuery, insertionColumns, insertArray);
+    return databaseResponse;
+};
+
+
+const createPersistableObject = (instruction) => ({
+    presentationType: instruction.presentationType,
+    active: true,
+    audienceType: instruction.audienceType,
+    templates: {
+        default: instruction.defaultTemplate,
+        otherTemplates: instruction.otherTemplates? instruction.otherTemplates: null
+    },
+    selectionInstruction: instruction.selectionInstruction? instruction.selectionInstruction: null,
+    recurrenceInstruction: instruction.recurrenceInstruction? instruction.recurrenceInstruction: null,
+    responseAction: instruction.responseAction? instruction.responseAction: null,
+    responseContext: instruction.responseContext? instruction.responseContext: null,
+    startTime: instruction.startTime? instruction.startTime: null,
+    endTime: instruction.endTime? instruction.endTime: null,
+    priority: instruction.priority? instruction.priority: 0
+});
+
 
 module.exports.evaluateMessageInstruction = (instruction) => {
     const requiredProperties = ['presentationType', 'audienceType', 'templates'];
