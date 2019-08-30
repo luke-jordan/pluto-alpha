adbdea10093b93fd9f93f751f7976b0bb9a55db4
luke-jordan <luke@plutosaving.com>
2019-08-30 15:45:29 +0200

    Adding a CORS module, to see if works

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -274,6 +274,15 @@ resource "aws_api_gateway_integration" "balance_fetch_wrapper" {
   uri                     = "${aws_lambda_function.balance_fetch_wrapper.invoke_arn}"
 }
 
+module "cors" {
+  source = "./modules/cors"
+  # version = "0.3.0"
+
+  api_id          = "${aws_api_gateway_rest_api.api_gateway.id}"
+  api_resource_id = "${aws_api_gateway_resource.balance_fetch_wrapper.id}"
+}
+
+
 /////////////// MESSAGING LAMBDAS //////////////////////////////////////////////////////////////////////////
 
 resource "aws_api_gateway_resource" "message_path_root" {
