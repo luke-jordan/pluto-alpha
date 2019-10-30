ed8a7a5b9250753f32a7e428d5c3f237bd8df989
Bolu Ajibawo <bolu.ajibawo@gmail.com>
2019-10-31 00:36:11 +0100

    Setup unit tests for base case on audience selection

---
+++ modules/audience-selection/test/unit.test.js
@@ -0,0 +1,113 @@
+'use strict';
+
+const chai = require('chai');
+// const sinon = require('sinon');
+const expect = chai.expect;
+
+const audienceSelection = require('../index');
+
+const rootJSON = {
+    "table": "transactions"
+};
+
+describe('Audience Selection', () => {
+
+    it('should be able to handle simple AND statements', async () => {
+        const mockSelectionJSON = Object.assign({}, rootJSON, {
+            "conditions": [{
+                 "op": "and", "children": [
+                     { "op": "is", "prop": "transaction_type", "value": "USER_SAVING_EVENT" },
+                     { "op": "is", "prop": "settlement_status", "value": "SETTLED" }
+                ]
+            }]
+        });
+
+        const expectedQuery = `select * from transactions where transaction_type='USER_SAVING_EVENT' and settlement_status = 'SETTLED'`;
+        const result = await audienceSelection.fetchUsersGivenJSON(mockSelectionJSON);
+
+        expect(result).to.exist;
+        expect(result).to.deep.equal(expectedQuery);
+    });
+
+    it('should be able to handle simple OR statements', async () => {
+        const mockSelectionJSON = Object.assign({}, rootJSON, {
+            "conditions": [{
+                "op": "or", "children": [
+                    { "op": "is", "prop": "transaction_type", "value": "USER_SAVING_EVENT" },
+                    { "op": "is", "prop": "settlement_status", "value": "SETTLED" }
+                ]
+            }]
+        });
+        const expectedQuery = `select * from transactions where transaction_type='USER_SAVING_EVENT' or settlement_status = 'SETTLED'`;
+        const result = await audienceSelection.fetchUsersGivenJSON(mockSelectionJSON);
+
+        expect(result).to.exist;
+        expect(result).to.deep.equal(expectedQuery);
+    });
+
+    it('should be able to handle simple AND and OR statements', async () => {
+        const mockSelectionJSON = Object.assign({}, rootJSON, {
+            "conditions": [{
+                "op": "or", "children": [
+                    { "op": "is", "prop": "creation_time", "value": "2019-01-27" },
+                    { "op": "or", "children": [
+                        { "op": "is", "prop": "transaction_type", "value": "USER_SAVING_EVENT" },
+                        { "op": "is", "prop": "settlement_status", "value": "SETTLED" }
+                    ]}
+                ]
+            }]
+        });
+
+        const expectedQuery = `select * from transactions where (transaction_type='USER_SAVING_EVENT' and settlement_status = 'SETTLED') or (creation_time >= '2019-01-27')`;
+        const result = await audienceSelection.fetchUsersGivenJSON(mockSelectionJSON);
+
+        expect(result).to.exist;
+        expect(result).to.deep.equal(expectedQuery);
+    });
+
+    it('should be able to handle complex AND and OR statements', async () => {
+        const mockSelectionJSON = Object.assign({}, rootJSON, {
+            "conditions": [{
+                "op": "or", "children": [
+                    { "op": "and", "children": [
+                        { "op": "is", "prop": "transaction_type", "value": "USER_SAVING_EVENT" },
+                        { "op": "is", "prop": "settlement_status", "value": "SETTLED" }
+                    ]},
+                    { "op": "and", "children": [
+                        { "op": "is", "prop": "creation_time", "value": "2019-01-27" },
+                        { "op": "is", "prop": "responsible_client_id", "value": "1" }
+                    ]}
+                ]
+            }]
+        });
+
+        const expectedQuery = `select * from transactions where (transaction_type='USER_SAVING_EVENT' and settlement_status = 'SETTLED') or (creation_time >= '2019-01-27' and responsible_client_id = 1)`;
+        const result = await audienceSelection.fetchUsersGivenJSON(mockSelectionJSON);
+
+        expect(result).to.exist;
+        expect(result).to.deep.equal(expectedQuery);
+    });
+
+    it('should be able to handle more complex AND and OR statements', async () => {
+        const mockSelectionJSON = Object.assign({}, rootJSON, {
+            "conditions": [{
+                "op": "and", "children": [
+                    { "op": "is", "prop": "responsible_client_id", "value": "1" },
+                    { "op": "or", "children": [
+                        { "op": "is", "prop": "creation_time", "value": "2019-01-27" },
+                        { "op": "and", "children": [
+                                { "op": "is", "prop": "transaction_type", "value": "USER_SAVING_EVENT" },
+                                { "op": "is", "prop": "settlement_status", "value": "SETTLED" },
+                            ]}
+                    ]}
+                ]
+            }]
+        });
+        const expectedQuery = `select * from transactions where ((transaction_type='USER_SAVING_EVENT' and settlement_status = 'SETTLED') or creation_time >= '2019-01-27') and responsible_client_id = 1`;
+        const result = await audienceSelection.fetchUsersGivenJSON(mockSelectionJSON);
+
+        expect(result).to.exist;
+        expect(result).to.deep.equal(expectedQuery);
+    });
+
+});
