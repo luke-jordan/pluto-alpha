fdf89752b5fc8f4f716af2a7ccbe3af630b6b5c8
luke-jordan <luke@plutosaving.com>
2020-02-27 15:14:40 +0200

    Adding referral defaults to verify on demand

--- functions/referral-api/test/referral.unit.test.js
+++ functions/referral-api/test/referral.unit.test.js
@@ -95,7 +95,8 @@ describe('*** UNIT TESTING CREATE REFERRAL CODE ***', () => {
 
     const userReferralDefaults = {
         boostAmountEach: '5::WHOLE_CURRENCY::USD',
-        fromBonusPoolId: 'primary_bonus_pool'
+        fromBonusPoolId: 'primary_bonus_pool',
+        shareLink: 'https://jupitersave.com'
     };
 
     const expectedDynamoInsertionUserCode = {
@@ -108,7 +109,8 @@ describe('*** UNIT TESTING CREATE REFERRAL CODE ***', () => {
         clientIdFloatId: 'some_client_co::primary_client_float',
         context: {
             boostAmountOffered: '5::WHOLE_CURRENCY::USD',
-            bonusPoolId: 'primary_bonus_pool'
+            bonusPoolId: 'primary_bonus_pool',
+            shareLink: 'https://jupitersave.com'
         }
     };
 
@@ -236,12 +238,14 @@ describe('*** UNIT TESTING VERIFY REFERRAL CODE ***', () => {
     const nonExistentCode = 'LETMEIN';
     const testCountryCode = 'RWA';
 
-    const desiredCols = ['referralCode', 'codeType', 'expiryTimeMillis', 'context'];
+    const desiredCols = ['referralCode', 'codeType', 'expiryTimeMillis', 'context', 'clientId', 'floatId'];
     const returnedCodeDetails = {
         referralCode: testBetaCode,
         countryCode: testCountryCode,
         codeType: 'BETA',
         expiryTimeMillis: moment().add(1, 'month').valueOf(),
+        clientId: 'someClient',
+        floatId: 'someFloat',
         context: {
             openingBonus: {
                 'amount': '5',
@@ -276,6 +280,21 @@ describe('*** UNIT TESTING VERIFY REFERRAL CODE ***', () => {
         const verificationBody = testHelper.standardOkayChecks(resultOfVerification);
         expect(verificationBody).to.deep.equal({ result: 'CODE_IS_ACTIVE', codeDetails: returnedCodeDetails });
         expect(fetchRowStub).to.have.been.calledWithExactly(activeCodeTable, { referralCode: testBetaCode, countryCode: testCountryCode }, sinon.match(desiredCols));
+    });
+
+    it('Happy path get float details as well', async () => {
+        const mockReferralDefaults = { shareLink: 'https://jupitersave.com/other' };
+        fetchRowStub.withArgs(activeCodeTable, { countryCode: testCountryCode, referralCode: testBetaCode }, sinon.match(desiredCols)).resolves(returnedCodeDetails);
+        fetchRowStub.withArgs(clientFloatTable, { clientId: 'someClient', floatId: 'someFloat' }, ['user_referral_defaults'])
+            .resolves({ userReferralDefaults: mockReferralDefaults });
+        const resultOfFetch = await handler.verify({ referralCode: testCodeCases, countryCode: testCountryCode, includeFloatDefaults: true });
+        const verificationBody = testHelper.standardOkayChecks(resultOfFetch);
+        
+        const expectedDetails = { ...returnedCodeDetails, floatDefaults: mockReferralDefaults };
+        expect(verificationBody).to.deep.equal({ result: 'CODE_IS_ACTIVE', codeDetails: expectedDetails });
+        expect(fetchRowStub).to.have.been.calledTwice;
+        expect(fetchRowStub).to.have.been.calledWithExactly(activeCodeTable, { referralCode: testBetaCode, countryCode: testCountryCode }, sinon.match(desiredCols));
+        expect(fetchRowStub).to.have.been.calledWithExactly(clientFloatTable, { clientId: 'someClient', floatId: 'someFloat' }, ['user_referral_defaults']);
     });
 
     it('Happy path referral code does not exist / is not active', async () => {
