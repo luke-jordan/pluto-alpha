9c5a90ad247aaeb33d19f4b6c7dfa769bf60eb0a
luke-jordan <luke@plutosaving.com>
2020-04-15 23:02:32 +0200

    Fixed TF bug

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -219,13 +219,13 @@ resource "aws_api_gateway_integration" "referral_verify" {
 resource "aws_api_gateway_resource" "referral_status" {
   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
   parent_id   = "${aws_api_gateway_resource.referral_path_root.id}"
-  path_part   = "verify"
+  path_part   = "status"
 }
 
 resource "aws_api_gateway_method" "referral_status" {
   rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
   resource_id   = "${aws_api_gateway_resource.referral_status.id}"
-  http_method   = "POST"
+  http_method   = "GET"
   authorization = "NONE"
 }
 
