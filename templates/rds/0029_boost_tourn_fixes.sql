e084da07a09d68ce23d64773f0f65ff3fe1b6abe
luke-jordan <luke@plutosaving.com>
2020-06-09 02:18:47 +0200

    Fixed constraint migration script

--- templates/rds/0029_boost_tourn_fixes.sql
+++ templates/rds/0029_boost_tourn_fixes.sql
@@ -6,7 +6,7 @@ grant select (updated_time) on transaction_data.core_transaction_ledger to frien
 
 alter table float_data.float_transaction_ledger drop constraint if exists float_transaction_type_check;
 alter table float_data.float_transaction_ledger add constraint float_transaction_type_check check (
-    t_type in ('ACCRUAL', 'ALLOCATION', 'USER_SAVING_EVENT', 'WITHDRAWAL', 'CAPITALIZATION', 'BOOST_REDEMPTION', 'ADMIN_BALANCE_RECON', 'BOOST_POOL_FUNDING')
+    t_type in ('ACCRUAL', 'ADMIN_ALLOCATION', 'USER_SAVING_EVENT', 'WITHDRAWAL', 'CAPITALIZATION', 'BOOST_REDEMPTION', 'ADMIN_BALANCE_RECON', 'BOOST_POOL_FUNDING')
 );
 
 alter table transaction_data.core_transaction_ledger drop constraint if exists account_transaction_type_check; 
