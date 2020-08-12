8ca6860cec28723fd991c51d158d17dd72a04de1
luke-jordan <luke@plutosaving.com>
2020-08-12 17:44:50 +0200

    Brought small migration in, now deploying

--- templates/rds/0038_boost_misc.sql
+++ templates/rds/0038_boost_misc.sql
@@ -1,6 +1,4 @@
--- to prevent running for now
-cause failure;
-
+-- allow for new statuses
 alter table boost_data.boost_account_status drop constraint if exists boost_account_status_boost_status_check;
 alter table boost_data.boost_account_status add constraint boost_account_status_boost_status_check check (
     boost_status in ('CREATED', 'OFFERED', 'PENDING', 'UNLOCKED', 'REDEEMED', 'CONSOLED', 'REVOKED', 'EXPIRED', 'FAILED')
