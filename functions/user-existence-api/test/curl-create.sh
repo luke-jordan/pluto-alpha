e8b95cb0d14951c8128b17f8ea1c96e1b0420ccb
luke-jordan <luke@plutosaving.com>
2020-09-06 18:10:18 +0200

    Removing apostrophes and spaces from surnames in account ref gen

--- functions/user-existence-api/test/curl-create.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-curl --header "Content-Type: application/json" --request POST   --data '{"ownerUserId": "2c957aca-47f9-4b4d-857f-a3205bfc6a78", "userFirstName": "Luke", "userFamilyName": "Jordan"}' http://localhost:4567/restapis/799A-Z979159/local/_user_request_/create
-
-# docker run --net="unnamed_default" --rm -v "$PWD":/var/task lambci/lambda:nodejs12.x handler.create '{"ownerUserId": "2c957aca-47f9-4b4d-857f-a3205bfc6a78"}' --network bridge
