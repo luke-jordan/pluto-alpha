faaef8e533b59f238214f347f93b1ee358ca7d9f
busani <busani@plutosave.com>
2020-09-14 13:02:29 +0200

    Improved locked-handler test coverage

--- functions/user-activity-api/test/locked.handler.unit.js
+++ functions/user-activity-api/test/locked.handler.unit.js
@@ -26,13 +26,14 @@ const handler = proxyquire('../locked-handler', {
 describe('*** UNIT TEST LOCKED SAVE BONUS PREVIEW ***', () => {
     const testSystemId = uuid();
 
-    const testFloatProjectionVars = {
-        accrualRateAnnualBps: 250,
-        lockedSaveBonus: { 30: 1.01, 60: 1.05, 90: 1.1 }
-    };
+    beforeEach(() => testHelper.resetStubs(fetchFloatVarsStub));
 
+    it('Calculates projected bonus for locked saves by number of locked days', async () => {
+        const testFloatProjectionVars = {
+            accrualRateAnnualBps: 250,
+            lockedSaveBonus: { 30: 1.01, 60: 1.05, 90: 1.1 }
+        };
 
-    it('Calculates projected bonus for locked saves by number of locked days', async () => {  
         fetchFloatVarsStub.resolves(testFloatProjectionVars);
 
         const testEventBody = {
@@ -58,6 +59,35 @@ describe('*** UNIT TEST LOCKED SAVE BONUS PREVIEW ***', () => {
         expect(fetchFloatVarsStub).to.have.been.calledOnceWithExactly('some_client', 'primary_cash');
     });
 
+    it('Uses default multilier where locked save bonus not in client-float vars', async () => {
+        const testFloatProjectionVars = { accrualRateAnnualBps: 250 };
+
+        fetchFloatVarsStub.resolves(testFloatProjectionVars);
+
+        const testEventBody = {
+            clientId: 'some_client',
+            floatId: 'primary_cash',
+            amountDetails: { amount: 10000, unit: 'HUNDREDTH_CENT', currency: 'USD' },
+            daysToPreview: [1, 4, 30, 67, 70]
+        };
+
+        const testEvent = testHelper.wrapEvent(testEventBody, testSystemId, 'ORDINARY_USER');
+
+        const previewResult = await handler.previewBonus(testEvent);
+        const resultBody = testHelper.standardOkayChecks(previewResult);
+
+        const expectedResult = {
+            '1': { amount: 152.53390581494563, unit: 'HUNDREDTH_CENT', currency: 'USD' },
+            '4': { amount: 624.2380778166955, unit: 'HUNDREDTH_CENT', currency: 'USD' },
+            '30': { amount: 5748.291920665036, unit: 'HUNDREDTH_CENT', currency: 'USD' },
+            '67': { amount: 17573.255999765744, unit: 'HUNDREDTH_CENT', currency: 'USD' },
+            '70': { amount: 18854.356857090857, unit: 'HUNDREDTH_CENT', currency: 'USD' }
+        };
+
+        expect(resultBody).to.deep.equal(expectedResult);
+        expect(fetchFloatVarsStub).to.have.been.calledOnceWithExactly('some_client', 'primary_cash');
+    });
+
     it('Handles invalid events and thrown errors', async () => {
         await expect(handler.previewBonus({ })).to.eventually.deep.equal({ statusCode: 400, body: 'Empty invocation' });
         await expect(handler.previewBonus({ clientId: 'some_client' })).to.eventually.deep.equal({ statusCode: 403 });
@@ -65,7 +95,7 @@ describe('*** UNIT TEST LOCKED SAVE BONUS PREVIEW ***', () => {
         fetchFloatVarsStub.throws(new Error('Dynamo error'));
         const testEvent = testHelper.wrapEvent({ clientId: 'some_client' }, testSystemId, 'ORDINARY_USER');
         const expectedResult = { statusCode: 500, headers: testHelper.expectedHeaders, body: JSON.stringify({ error: 'Dynamo error' }) };
-        
+
         await expect(handler.previewBonus(testEvent)).to.eventually.deep.equal(expectedResult);
     });
 });
